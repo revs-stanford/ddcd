f2c54661a885aaa3b079fc3548f4687b0f144e50
Dan Nguyen <dansonguyen@gmail.com>
2016-12-04 19:16:35 -0800

    hi

--- config.rb
+++ config.rb
@@ -51,7 +51,7 @@ activate :s3_sync do |s3_sync|
   s3_sync.bucket                     = 'www.datadrivenstanford.org' # The name of the S3 bucket you are targetting. This is globally unique.
   s3_sync.region                     = 'us-west-1'     # The AWS region for your bucket.
   s3_sync.delete                     = false # We delete stray files by default.
-  s3_sync.after_build                = true # We do not chain after the build step by default.
+#  s3_sync.after_build                = true # We do not chain after the build step by default.
   s3_sync.prefer_gzip                = false
   s3_sync.path_style                 = true
   s3_sync.reduced_redundancy_storage = false
